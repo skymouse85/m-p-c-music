28b64bd2e7ae4b6c2d20345dd116248e7b1b2fe0
Mark Clifford <markmclifford@gmail.com>
2023-04-29 20:24:28 -0700

    initial layout

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import { Raleway } from "next/font/google";
+import styles from '@/styles/Home.module.css'   
+
+
+export default function Header() {
+    return (
+        <>
+            <div>Mark Pascucci-Clifford Music</div>
+            <nav>
+                <ul>
+                    <li>bio</li>
+                    <li>about</li>
+                    <li>project</li>
+                    <li>links  </li>
+                </ul>
+            </nav>
+
+        
+        
+        </>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import { Raleway, Roboto_Mono, Inter } from 'next/font/google'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
