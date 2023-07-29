1cceb4aeda47069d1e361e606e065d3c240c5fc7
Mark Clifford <markmclifford@gmail.com>
2023-07-28 17:08:20 -0700

    cal updates

--- pages/performances.js
+++ pages/performances.js
@@ -7,6 +7,11 @@ function EventList() {
     const [events, setEvents] = useState([]);
     const [error, setError] = useState(null);
 
+
+    // function dateTimeParse (
+
+    // )
+
     useEffect(() => {
         fetch('http://localhost:8080') // Make sure the URL is correct
             .then(response => response.json())
@@ -25,17 +30,20 @@ function EventList() {
     if (error) {
         return <div>Error: {error}</div>;
     }
-
+//will need to map backwards to show shows in order
     return (
         <>
              <div className={homeStyles.sm_header}>
                 <SmHeader />  
             </div>
                 <div>
+                
                     {events.map(event => (
                         <div key={event.id}>
                             <h2>{event.summary}</h2>
-                            <p>{event.start.dateTime} - {event.end.dateTime}</p>
+                            <p>{event.description}</p>
+                            <p>{event.location}</p>
+                            <p>{event.start?.dateTime} - {event.end?.dateTime}</p>
                         </div>
                     ))}
                 </div>
